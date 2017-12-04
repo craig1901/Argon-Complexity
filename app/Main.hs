de4f78bd315de6cb18408f034f3aee0de058176d
Craig Nolan <nolancr@tcd.ie>
2017-12-04 14:19:36 +0000

    can now split up work file by file

--- app/Main.hs
+++ app/Main.hs
@@ -4,23 +4,33 @@ import Lib
 import Control.Distributed.Process
 import Control.Distributed.Process.Backend.SimpleLocalnet
 import Control.Distributed.Process.Closure
-import Control.Distributed.Process.Node                   (initRemoteTable)
+import Control.Distributed.Process.Node (initRemoteTable)
 import Control.Monad
-import Network.Transport.TCP                              (createTransport, defaultTCPParameters)
-import System.Environment                                 (getArgs)
+import Network.Transport.TCP (createTransport, defaultTCPParameters)
+import System.Environment (getArgs)
 import System.Exit
-import System.FilePath
-import System.Directory
+import System.FilePath ((</>))
+import System.Directory (doesDirectoryExist, getDirectoryContents, getCurrentDirectory)
 
+getRecursiveContents :: FilePath -> IO [FilePath]
+getRecursiveContents topdir = do
+    names <- getDirectoryContents topdir
+    let properNames = filter (\f -> head f /= '.' && f /= "argon") names
+    paths <- forM properNames $ \name -> do
+        let path = topdir </> name
+        isDirectory <- doesDirectoryExist path
+        if isDirectory
+            then getRecursiveContents path
+            else return [path]
+    return (concat paths)
 
-getFiles :: FilePath -> IO [FilePath]
-getFiles path = fmap (\f -> path ++ "/" ++ f) . filter (\p -> head p /= '.' && p /= "argon") <$> getDirectoryContents path
 
 main :: IO ()
 main = do
     args <- getArgs
     curr <- getCurrentDirectory
-    files <- getFiles curr
+    -- files <- getFiles curr
+    files <- getRecursiveContents curr
     case args of
       ["manager", host, port] -> do
         putStrLn "Starting Node as Manager"
--- workers.sh
+++ workers.sh
@@ -1,2 +1,5 @@
 stack exec complexity-api worker localhost 8000 &
-stack exec complexity-api worker localhost 8001 
+stack exec complexity-api worker localhost 8001 &
+stack exec complexity-api worker localhost 8002 &
+stack exec complexity-api worker localhost 8003 &
+stack exec complexity-api worker localhost 8004 
